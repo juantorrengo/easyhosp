ae978a94c9066fdd616b47a618187a189bacf8b0
juantorrengo <jmtorrengo@gmail.com>
2016-05-24 11:37:10 -0300

    se agregó una vista básica

---
+++ web/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('#login').on('click', function () {
+        $('#login-modal').modal();
+    });
+
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    });
+});
