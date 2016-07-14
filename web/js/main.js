dc9d11ebf3f6f2d5a4853c98af008e9d716df8d0
juantorrengo <jmtorrengo@gmail.com>
2016-07-14 16:28:35 -0300

    se eliminó el campo "reservadas" de la entidad Hospedaje

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,28 @@
+var dataTableConf = {
+    "oLanguage": {
+        "oPaginate": {
+            "sPrevious": "Anterior",
+            "sNext": "Siguiente",
+            "sLast": "Ultima",
+            "sFirst": "Primera"
+        },
+        "sLengthMenu": 'Mostrar <select>'+
+        '<option value="10">10</option>'+
+        '<option value="20">20</option>'+
+        '<option value="30">30</option>'+
+        '<option value="40">40</option>'+
+        '<option value="50">50</option>'+
+        '<option value="-1">Todos</option>'+
+        '</select> registros',
+        "sInfo": "Mostrando del _START_ a _END_ (Total: _TOTAL_ resultados)",
+        "sInfoFiltered": " - filtrados de _MAX_ registros",
+        "sInfoEmpty": "No hay resultados de búsqueda",
+        "sZeroRecords": "No hay registros a mostrar",
+        "sProcessing": "Espere, por favor...",
+        "sSearch": "Buscar:",
+    }
+};
+
 $(document).ready(function() {
     $('.carousel').carousel({
         interval: 5000 //changes the speed
